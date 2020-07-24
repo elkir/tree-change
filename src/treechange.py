cc5e0aafabd05282698177861f7f5175dc98fb75
Petr Doležal <sirelkir@gmail.com>
2020-07-24 06:51:14 +0200

    Created template for main treechange object

---
+++ src/treechange.py
@@ -0,0 +1,48 @@
+
+
+
+
+class TreeChange():
+
+
+
+
+    def __init__(self):
+
+
+        self.load
+        self.y1
+        self.y2
+        self.r1
+        self.r2
+        self.df
+
+
+
+    def gather_all_runs(self):
+
+
+    def load_data(self):
+        #TODO
+        pass
+
+
+    #TODO generate Diff raster?
+
+    def find_missing_trees(self):
+        pass
+
+    def match_trees(self):
+        #TODO
+        pass
+
+    def plot_raster(self):
+        pass
+
+    def plot_tree(self):
+        #TODO
+        pass
+
+    def plot_tree_hist(self):
+        #TODO
+        pass
