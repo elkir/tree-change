ed29f5c81accde80085c71b1adacdf146f45e099
Petr Doležal <sirelkir@gmail.com>
2020-07-25 10:30:45 +0200

    Added the TreeChange class and reimplemented it's workflow.
    In progress

---
+++ development_python/11_test_treechange_class.py
@@ -0,0 +1,186 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import os
+from pathlib import Path
+
+# vizualization
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+dpi = 300
+matplotlib.rcParams['figure.dpi'] = dpi
+markersize = 0.5*(300/dpi)
+
+# GIS
+import fiona
+import fiona.crs
+import shapely
+import rasterio
+import rasterio.plot
+import rasterio.mask
+import geopandas as gpd
+
+
+from scipy.spatial import cKDTree
+
+from src.mycolors import rand_cmap
+from src.loading import Load
+from src.plots import plot_tree,plot_raster_polygon,plot_tree_hist
+from src.mycolors import rand_cmap
+from src.treechange import TreeChange
+
+#%%
+try:
+    dirpath = Path(os.path.dirname(__file__))
+except:
+    dirpath = Path(os.getcwd())/"development_python"
+
+dir_data = Path(f"{dirpath}/../Data/lidar")
+#%%
+tc = TreeChange(dir_data,(2013,2014))
+
+tc.print(folders_all=True)
+
+
+#%%
+print("Gather runs")
+tc.gather_all_runs(print_validity=True)
+
+#%%
+print("Load and match trees")
+
+params = tc.runs_index.sample().iloc[0]
+tc.load_data(params) #create_diff=True if needed
+tc.match_trees()
+
+tc.print()
+
+
+#
+# print(tc._tt)
+# print(tc._cr)
+# print(tc.chm)
+
+#%% Random colormap definition
+rcmap = rand_cmap(1000,'bright',verbose=False)
+
+#%% Plot absolute difference between nn and nn2
+dim = 16
+fig,ax = plt.subplots(figsize=(dim,dim))
+im = tc.df.plot(ax=ax,column='diff',cmap='magma_r')
+# tc._tt[0].plot(ax=ax,marker='+',markersize=markersize,color='r')
+# tc._tt[1].plot(ax=ax,marker='+',markersize=markersize,color='b')
+plt.colorbar(im.collections[0],ax=ax)
+fig.suptitle("Distance to neigbours")
+plt.show()
+
+#%% Plot trees based on threshold of nn and nn2
+
+fig,ax = plt.subplots(figsize=(4,4))
+im = tc.df.plot(ax=ax,column='i_diff',cmap="Paired_r")
+tc._tt[0].plot(ax=ax,marker='+',markersize=markersize,color='r')
+tc._tt[1].plot(ax=ax,marker='+',markersize=markersize,color='b')
+# plt.colorbar(im.collections[0],ax=ax)
+plt.suptitle("Trees with good neighbours")
+plt.show()
+
+
+
+#
+# #%% Select some tree
+# # trees_df = cr[0][cr[0]['DN']==700]
+# trees_df = cr[0].iloc[[cr[0].geometry.area.argmax()]] # biggest tree
+# # trees_df = cr[0].iloc[[300]]
+# tree=trees_df.iloc[0].geometry
+#
+# fig,ax = plt.subplots(figsize=(12,12))
+# im = rasterio.plot.show(chm_arr[0],transform=chm[0].transform,ax=ax)
+# trees_df.geometry.boundary.plot(ax=ax,color='r',alpha=1,linewidth=1)
+# # plt.colorbar(im,ax=ax)
+# plt.suptitle("Selected tree(s) (biggest?)")
+# fig.show()
+#
+#
+#
+# #%% Get tree data and plot it
+#
+# # TODO write a function that extracts a tree
+# r=chm[0]
+# fig,ax = plt.subplots()
+# plot_tree(r,tree,ax=ax)
+# fig.suptitle("Cutout selected tree")
+# fig.show()
+# # rasterio.plot.show_hist(out_image)
+#
+#
+#
+# #%% plot difference raster
+# fig,ax = plt.subplots(figsize=(12,12))
+# im= ax.imshow(chm_diff,cmap='RdBu_r',vmin=-80,vmax=80)
+# plt.colorbar(im,ax=ax)
+# fig.suptitle("Difference between rasters")
+# fig.show()
+#
+# #%% plot a single tree diference raster
+# r=chm_diff_ds
+# fig,ax = plt.subplots()
+# plot_tree(r,tree,ax=ax,
+#           param_boundary=dict(color='m'),
+#           param_box=dict(cmap='RdBu_r',vmin=-80,vmax=80),
+#           param_polygon=dict(cmap='RdBu_r',vmin=-80,vmax=80))
+# # plt.colorbar(cax=im,ax=ax) # TODO
+# fig.suptitle("Selected tree on the difference raster")
+# fig.show()
+#
+# #%%  plot a single tree histogram
+# plot_tree_hist(r,tree)
+#
+# #%% Find change in tree height #TODO
+#
+# # a= out_image
+# # # filter outliers
+# # q = np.quantile(out_image,[0.1,0.9])
+# # a_reduced = a[(a>q[0]) & (a<q[1])]
+# # a_stats = stats.describe(a_reduced)
+#
+# #%% add cols to dataframe
+#
+# # treeID = 6
+# for treeID in [300]:
+#
+#     tree= tt[0][tt[0].treeID==treeID]
+#     crown=crowns[crowns.DN==treeID].iloc[0]
+#     poly = crown.geometry
+#     poly=poly
+#     print(poly)
+#     # plt.plot(*poly.exterior.xy)
+#     # if poly.interiors is not None:
+#     #     for ring in poly.interiors:
+#     #         plt.plot(*ring.xy)
+#     # plt.show()
+#
+#
+# #%%
+#
+# (
+# crowns.geometry
+#     .apply(lambda x: shapely.geometry.Polygon(x.exterior))
+#     .apply(lambda x: len(x.interiors)==0)
+#     .describe()
+# )
+#
+# #%%
+# crowns = crowns.rename(columns={"DN":"treeID"}).set_index("treeID")
+#
+# #%%
+# # def sphericity(polygon):
+# #     length =
+# #     area =
+#
+
+
+
--- src/treechange.py
+++ src/treechange.py
@@ -1,48 +1,247 @@
 
 
+from collections import namedtuple
+from io import StringIO
+
+from functools import partial
+import numpy as np
+import pandas as pd
+from scipy import stats
+import os
+from pathlib import Path
+
+# vizualization
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+dpi = 300
+matplotlib.rcParams['figure.dpi'] = dpi
+markersize = 0.5*(300/dpi)
+
+# GIS
+import fiona
+import fiona.crs
+import shapely
+import rasterio
+import rasterio.plot
+import rasterio.mask
+import geopandas as gpd
+
+
+from scipy.spatial import cKDTree
+
+from src.mycolors import rand_cmap,txt_sty
+from src.loading import Load
+from src.plots import plot_tree,plot_raster_polygon,plot_tree_hist
+
+
+Pair = namedtuple('Pair',"old new")
 
 
 class TreeChange():
 
+    def __init__(self,dir_data,years): #TODO infer years
+
+        self.load = Load(dir_data)
+        self.runs_index = None
+        self.year = Pair(*years)
+        self.chm = None
+        self.diff = None
+        self.df = None
+        self._cr = None
+        self._tt = None
+        # pure raster arrays TODO do they need to be created? A namedtuple alternative?
+        self._chm_arr = None
+        self._diff_arr = None
+
+        self.df_params = None
+        #flags
+        self.flag_neighbours = False
+
+    # def __repr__(self):
+    #     #TODO with new lines
+    #     ...
+
+    def print(self,folders_all=False):
+
+        # define string buffer and redefine print to print to it
+        buf = StringIO()
+        printb = partial(print,file=buf)
+
+        printb(f"{txt_sty['BOLD']}Tree Comparison Object{txt_sty['END']}")
+        printb(f"Data (dir_data):\t{self.load.dir_data}")
+        if folders_all:
+            for k,v in vars(self.load).items():
+                if k.startswith(('dir','ff')) and k!="dir_data":
+                    try:
+                        v = v.relative_to(self.load.dir_data)
+                    except ValueError:
+                        continue
+                    printb(f"\t\t{k+':':20} {v}")
+
+        printb(f"Runs:")
+        if self.runs_index is None:
+            printb(f"\tRuns not loaded")
+        else:
+            printb(f"\tNumber: {len(self.runs_index)}")
+            printb(f"\tWS: {self.runs_index.ws.value_counts().sort_index().to_dict().__repr__()}")
+
+        printb(f"Years:\t{self.year or 'Not specified yet'}")
+
+        printb(f"Rasters:")
+        printb(f"\t{self.chm if self.chm is not None else 'Not loaded'}")
+
+        printb(f"Dataframe:")
+        if self.df is None:
+            printb(f"\tNot constructed")
+        else:
+            printb(f"\tRows:\t {self.df.shape[0]}")
+            printb(f"\tColumns:\t {self.df.shape[1]}")
+            printb(f"\t\t{self.df.columns.to_list()}")
+
+
+        #TODO check whether all status is printed
+        print(buf.getvalue())
 
 
 
-    def __init__(self):
+    def gather_all_runs(self,print_validity=False,valid_only=True, return_value=True):
+        self.runs_index = pd.read_csv(self.load.ff_index_crowns,
+                                      sep=" ", names=["ws", "seed", "cr", "max"],
+                                      index_col=False)
+        i_seed_less_than_cr = self.runs_index['seed'] < self.runs_index['cr']
+
+        if print_validity:
+            i_ws20 = self.runs_index['ws'] == 20
+            i_valid = i_ws20 & i_seed_less_than_cr
+            a = np.sum(i_ws20)
+            b = np.sum(i_valid)
+            print(f"Runs with ws=20: {a}")
+            print(f"Of those valid: {b}")
+            print(f"Ratio {b/a}")
+        if valid_only:
+            self.runs_index = self.runs_index[i_seed_less_than_cr].reset_index()
+        print("Index of runs computed.")
+
+        return self.runs_index if return_value else None
 
 
-        self.load
-        self.y1
-        self.y2
-        self.r1
-        self.r2
-        self.df
 
 
 
-    def gather_all_runs(self):
 
+    def load_data(self,params,create_diff=False,keep_interior=False):
+        self.df_params = params
+        self._tt = Pair(*(self.load.load_tt(y, self.df_params['ws']) for y in self.year))
+        self._cr = Pair(*(self.load.load_cr(y, self.df_params) for y in self.year))
+        self.chm = Pair(*(self.load.load_chm(y) for y in self.year))
+        self._chm_arr = Pair(*(x.read(1,masked=True) for x in self.chm))
+        try:
+            self.diff = self.load.load_diff()
+        except FileNotFoundError:
+            print("Diff didn't exist on disk and couldn't be loaded. "
+                  "Set createNewDiff True when loading to create it.")
+            if create_diff:
+                self.diff = self.createDiff()
 
-    def load_data(self):
-        #TODO
-        pass
+        self.df = self._cr.old.copy()
+        self.df = self.df.rename(columns={"DN": "treeID"}).set_index("treeID")
+        if not(keep_interior):
+            self.df['geometry'] = self.df.geometry.exterior
 
+    def create_diff(self):
+        ff_diff = self.load.dir_diff / f"diff.tif"
+        diff_arr = self._chm_arr.old - self._chm_arr.new
 
-    #TODO generate Diff raster?
+        with rasterio.open(
+                f"{self.load.dir_chm}/diff.tif", 'w', driver="GTiff",
+                **{k: self.chm[0].meta[k] for k in (
+                'width', 'height', 'nodata', 'crs', 'transform', 'count', 'dtype')}
+        ) as ds:
+            ds.write(diff_arr, 1)
+
+        return self.load.load_diff()
 
     def find_missing_trees(self):
-        pass
+        ...
 
-    def match_trees(self):
+    def match_trees(self, overwrite=False):
+        def cKDnearest2(gdfA, gdfB):
+            nA = np.array(list(gdfA.geometry.apply(lambda x: (x.x, x.y))))
+            nB = np.array(list(gdfB.geometry.apply(lambda x: (x.x, x.y))))
+            btree = cKDTree(nB)
+            dist, idx = btree.query(nA, k=2)
+            gdf = pd.concat(
+                [gdfA.reset_index(drop=True),
+                 gdfB.loc[idx[:, 0], gdfB.columns != 'geometry'].reset_index(drop=True),
+                 pd.Series(dist[:, 0], name='dist'),
+                 gdfB.loc[idx[:, 1], gdfB.columns != 'geometry'].reset_index(drop=True),
+                 pd.Series(dist[:, 1], name='dist2')], axis=1)
+            return gdf
+
+        # def ckdnearest2_alt(gdA, gdB):
+        #     nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
+        #     nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
+        #     btree = cKDTree(nB)
+        #     dist, idx = btree.query(nA, k=2)
+        #     nn1 = pd.concat([gdB.loc[idx[:, 0], gdB.columns != 'geometry'].reset_index(drop=True),
+        #                      pd.Series(dist[:, 0], name='dist')], axis=1)
+        #     nn2 = pd.concat([gdB.loc[idx[:, 1], gdB.columns != 'geometry'].reset_index(drop=True),
+        #                      pd.Series(dist[:, 1], name='dist2')], axis=1)
+        #     gdf = pd.concat(
+        #         [gdA.reset_index(drop=True), nn1, nn2], axis=1, keys=["orig", "nn", "nn2"])
+        #     return gdf
+        #
+
+
+
         #TODO
-        pass
+        if not(self.flag_neighbours) or overwrite:
+            nn = cKDnearest2(*self._tt)
+            diff = nn['dist2'] - nn['dist']
+            i_diff = diff > 9 #TODO infer boundary
+            neighbours = pd.DataFrame(dict(nn=nn['dist'], nn2=nn['dist2'], diff=diff, i_diff=i_diff))
+            if overwrite:
+                self.df.update(neighbours,overwrite=True)
+            else:
+                self.df = pd.concat([self.df,neighbours], axis=1)
+                self.flag_neighbours=True
 
+    # Plots
     def plot_raster(self):
-        pass
+        ...
 
     def plot_tree(self):
         #TODO
-        pass
+        ...
 
     def plot_tree_hist(self):
         #TODO
-        pass
+        ...
+
+    def characterize_polygons(self):
+
+
+        area
+        perimeter
+        convex_perimeter
+        convex_area
+        major_axis
+        minor_axis
+        compactness
+        eccentricity
+        circularity
+        sphericity
+        convexity
+        curl #fibre length?
+        solidity
+        circular_variance # different from sphericity?
+        bending_energy
+
+        major_axis_angle
+
+        #requires values
+        moments
